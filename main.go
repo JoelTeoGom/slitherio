a51c2fe4141aae6b8bcccc60415640c667ddd1f1
Joel Teodoro <joelteodorogomez@gmail.com>
2024-09-14 00:06:59 +0200

    add snake + player

--- main.go
+++ main.go
@@ -17,20 +17,59 @@ func newGame() {
 	// Inicializar la variable global
 	game = new(GameState)
 	game.GameOver = false
-	game.GameWidth = 800
-	game.GameHeight = 450
-	game.PlayerSize = 40
+	game.GameWidth = 1000
+	game.GameHeight = 500
+	game.PlayerSize = 50
+}
+
+type Snake struct {
+	Length   int
+	Position []rl.Rectangle
+}
+
+var player *Player
+
+type Player struct {
+	ID          int
+	SnakePlayer Snake
+	Camera      rl.Camera2D
+	Direction   int
+}
+
+var playerCount = 0
+
+func newPlayer() {
+	player = new(Player)
+	player.SnakePlayer.Length = 1
+	player.ID = playerCount + 1
+	player.Direction = 1
 }
 
 func main() {
 
 	newGame()
+	newPlayer()
+
 	rl.InitWindow(game.GameWidth, game.GameHeight, "classic game: snake")
 	rl.SetTargetFPS(60)
 
 	if !game.GameOver {
-
 		for !rl.WindowShouldClose() {
+
+			if rl.IsKeyDown(rl.KeyA) {
+				player.Direction += -1
+			} //left
+			if rl.IsKeyDown(rl.KeyD) {
+				player.Direction += 1
+			} //right
+			if rl.IsKeyDown(rl.KeyW) {
+				player.Direction += 2
+			} //up
+			if rl.IsKeyDown(rl.KeyS) {
+				player.Direction += -2
+			} //down
+
+			drawSnake()
 
 			rl.BeginDrawing()
 
@@ -49,13 +88,17 @@ func main() {
 
 // funcion that prints the map
 func drawTable() {
-
 	for i := 0; i < int(game.GameWidth)/game.PlayerSize; i++ {
 		rl.DrawLineV(rl.Vector2{float32(i) * float32(game.PlayerSize), 0}, rl.Vector2{float32(i) * float32(game.PlayerSize), float32(game.GameHeight)}, rl.Black)
 	}
-
 	for i := 0; i < int(game.GameHeight)/game.PlayerSize; i++ {
 		rl.DrawLineV(rl.Vector2{0, float32(i) * float32(game.PlayerSize)}, rl.Vector2{float32(game.GameWidth), float32(i) * float32(game.PlayerSize)}, rl.Black)
 	}
+}
 
+func drawSnake() {
+
+	for i := 0; i < player.SnakePlayer.Length; i++ {
+
+	}
 }
